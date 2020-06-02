7f7458b1e5eefc4b5c123941d614834a26670944
Stratos-Chen <twinschen55@gmail.com>
2020-06-02 02:21:33 -0700

    updated routes with parameter searches

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -3,4 +3,16 @@ class Api::ActorsController < ApplicationController
     @display = Actor.find_by(id: 1)
     render "display_actor.json.jb"
   end
+
+  def find_actor_query
+    @id = params['id']
+    @display = Actor.find_by(id: @id)
+    render "display_actor.json.jb"
+  end
+
+  def find_actor_segment
+    @id = params[:id]
+    @segment = Actor.find_by(id: @id)
+    render "actors_segment.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   namespace :api do
     get "/actor_list" => "actors#find_actor"
     get "/movie_list" => "movies#find_movie"
+    get "/display_actor" => "actors#find_actor_query"
+    get "/display_actor2/:id" => "actors#find_actor_segment"
+    post "/display_actor3" => "actors#find_actor_query"
   end
 end

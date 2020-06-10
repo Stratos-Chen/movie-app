7b45d636c5b205d5528aa4ac14df4b0d2d1eb6a0
Stratos-Chen <twinschen55@gmail.com>
2020-06-10 07:05:16 -0700

    added assocation methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   validates :title, :year, :plot, :director, presence: true
   validates :title, :plot, uniqueness: true
   validates :year, numericality: { greater_than: 1970}
+  
+  has_many :actors
 end
